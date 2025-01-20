9ed0df8e487ad549776c1acefd9bb9a33c398904
iankusmon <muh.rahardian.k@gmail.com>
2025-01-20 08:52:42 +0700

    fix: get back to src/configs/axios-configs.js to axios baseURL

--- src/configs/axios-configs.js
+++ src/configs/axios-configs.js
@@ -9,7 +9,7 @@ axios.defaults.xsrfHeaderName  = 'X-CSRF-Token'
 axios.defaults.withCredentials = true
 
 // API Base URL
-axios.defaults.baseURL = 'https://andaraimperialterrace.click'
+axios.defaults.baseURL = 'http://localhost:3000'
 
 // axios.interceptors.request.use((request) => {
 //   console.log('Starting Request', request)
